09aaa264978e68d29e5093d74e1f80feb2d3f857
serena costantine <serenacostantine05@icloud.com>
2024-09-03 16:09:14 +0300

    excel file name

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
-
 //require('dotenv').config() //from npm dotenv: bas katabneha lamma naamel node app.js aal cmd la7 yekhod port 5555
 const PORT = process.env.PORT || 3030;
+
+const xlsx = require("xlsx");
+let wb = xlsx.readFile('excel.xlsx');
+let wh = wb.Sheets['Sheet1'];
+let _data = xlsx.utils.sheet_to_json(wh);
+
 
 // your code
 //from npm: express
@@ -11,15 +16,20 @@ app.get('/', function (req, res) {
   })
 
 //another one:men ba3ed ma chtaghalna hawdouk zedna hayde w kafayna cheghel
+//  app.get('/students', function (req, res) {
+//   var data = [
+//     {id:1 , name:'Serena'},
+//     {id:2 , name:'Maria'},
+//     {id:3 , name:'Titi'},
+//     {id:4 , name:'Mar'}
+//   ]
+//   res.send(data) //bas nekteba menjarreba 3a node app.js then: http://localhost:3030/students -> google chrome
+// })
+
 app.get('/students', function (req, res) {
-  var data = [
-    {id:1 , name:'Serena'},
-    {id:2 , name:'Maria'},
-    {id:3 , name:'Titi'},
-    {id:4 , name:'Mar'}
-  ]
-  res.send(data) //bas nekteba menjarreba 3a node app.js then: http://localhost:3030/students -> google chrome
+  res.send(_data)
 })
+
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
